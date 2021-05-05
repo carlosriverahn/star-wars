3571085219d0f0cdcaab0866fa605ee578db4e84
carlosriverahn <carlosriverahn@gmail.com>
2021-05-05 22:19:01 +0200

    Style settings in components and views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PeopleComponent } from './views/people/people.component';
 import { LoginComponent } from './views/home/login/login.component';
 import { SignUpComponent } from './views/home/sign-up/sign-up.component';
 import { ListStarShipsComponent } from './views/list-star-ships/list-star-ships.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ListStarShipsComponent } from './views/list-star-ships/list-star-ships.
     PeopleComponent,
     LoginComponent,
     SignUpComponent,
-    ListStarShipsComponent
+    ListStarShipsComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
